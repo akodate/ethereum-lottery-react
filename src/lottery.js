fb445febc5f448fc234afd08d92433154b6e4c21
akodate <alex.kodate@gmail.com>
2018-08-10 09:10:36 +0800

    add basic ability to interact with deployed contract

---
+++ src/lottery.js
@@ -0,0 +1,6 @@
+import web3 from './web3';
+
+const address = '0x05F7c5BEFfE35147d977336c87D0a44AdF810084';
+const abi = '[{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"random","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]';
+
+export default new web3.eth.Contract(JSON.parse(abi), address);
